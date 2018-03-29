97bd1595ddc9384f720518465af58755ea01ccd5
Victoria <victoria@some-mail.com>
2018-03-29 01:06:29 -0700

    still trying to fix images for styles...

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Jumbotron.css";
 
+const sakura = require('./sakura.png');
+
+const divStyle = {
+  backgroundImage: 'url(' + sakura + ')'
+};
+
 const Jumbotron = () => (
-  <div className="jumbotron text-center">
+  <div className="jumbotron text-center" style={divStyle}>
     <h1>Clicky-Cat</h1>
     <h3>Click on an image to earn points, but don't click on any more than once!</h3>
   </div>
